ce474f4ff17caafb68853770eb9164c229168a5c
JudithGo <39970801+JudithGo@users.noreply.github.com>
2019-02-11 10:35:22 +0100

    Update BowlingTest.php

--- tests/BowlingTest.php
+++ tests/BowlingTest.php
@@ -8,4 +8,28 @@ class BowlingTest extends \PHPUnit\Framework\TestCase
     {
         $this->sut = new Bowling();
     }
+    
+      /**
+     * @test 
+     */
+    public function allXMustReturn300()
+    {
+        $this->assertEquals(300, $this->sut->convert('xxxxxxxxxxxx'));
+    }
+
+    /**
+     * @test 
+     */
+    public function MissMustReturn90()
+    {
+        $this->assertEquals(90, $this->sut->convert('9-9-9-9-9-9-9-9-9-9-'));
+    }
+
+    /**
+     * @test 
+     */
+    public function spareMustReturn150()
+    {
+        $this->assertEquals(150, $this->sut->convert('5/5/5/5/5/5/5/5/5/5/5'));
+    }
 }
